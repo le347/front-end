496b5b98d8998eb157eb69cd56bec815cd1cae90
alfredcal <alfred.le@dynatrace.com>
2021-03-18 19:41:44 +0000

    green topbar for good version :D

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var request      = require("request")
 global.acmws = {};
 global.acmws['request-latency'] = 0;
 global.acmws['request-latency-catalogue'] = 500;
-global.acmws['response-error-probability'] = 20;
+global.acmws['response-error-probability'] = 0;
 
 
 app.use(helpers.rewriteSlash);
